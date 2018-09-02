bf21992bd553db469a5f29f82872460e8438c123
Daniel Phillimore <dan@ovms.co>
2018-09-02 19:17:39 +0100

    Add loader for the "any" type so it can be used in ACT YAML

---
+++ php/src/Combyna/Component/Type/Config/Loader/AnyTypeLoader.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Combyna
+ * Copyright (c) the Combyna project and contributors
+ * https://github.com/combyna/combyna
+ *
+ * Released under the MIT license
+ * https://github.com/combyna/combyna/raw/master/MIT-LICENSE.txt
+ */
+
+namespace Combyna\Component\Type\Config\Loader;
+
+use Combyna\Component\Config\Loader\ConfigParser;
+use Combyna\Component\Type\AnyType;
+
+/**
+ * Class AnyTypeLoader
+ *
+ * @author Dan Phillimore <dan@ovms.co>
+ */
+class AnyTypeLoader implements TypeTypeLoaderInterface
+{
+    /**
+     * @var ConfigParser
+     */
+    private $configParser;
+
+    /**
+     * @param ConfigParser $configParser
+     */
+    public function __construct(ConfigParser $configParser)
+    {
+        $this->configParser = $configParser;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function load(array $config)
+    {
+        return new AnyType();
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getTypes()
+    {
+        return ['any'];
+    }
+}
